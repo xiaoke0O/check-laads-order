a34097356b12da65d174277382baa2435641d5b4
Zheng Xueke <lengfeng1453@163.com>
2021-10-22 22:20:42 +0800

    再解决一些跨平台问题

--- src/check_laads_order.cpp
+++ src/check_laads_order.cpp
@@ -1,10 +1,11 @@
 #include "check_laads_order.h"
 #include "./ui_check_laads_order.h"
 #include "fast_cksum.h"
-#include<QAction>
+#include <QAction>
 #include <QDebug>
-#include <inttypes.h>
 #include <stdlib.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
 
 
 check_laads_order::check_laads_order(QWidget *parent)
@@ -13,10 +14,18 @@ check_laads_order::check_laads_order(QWidget *parent)
 {
     ui->setupUi(this);
     connect(ui->actionAbout_Qt,&QAction::triggered,qApp,&QApplication::aboutQt);
-    const char *filename = "/mnt/c/Users/xueke/Downloads/Music/aaa.ts";
+#ifdef __MINGW32__
+    const char *filename = QString("C:\\Users\\xueke\\Pictures\\微信图片_20210918235156.jpg").toLocal8Bit();
+#else
+        const char *filename = "C:\\Users\\xueke\\Pictures\\微信图片_20210918235156.jpg";
+#endif
     FILE *fp;
+#ifdef _MSC_VER
+    if(fopen_s(&fp,filename,"rb"))
+#else
     fp=fopen(filename, "rb");
     if (!fp)
+#endif
     {
         fprintf(stderr, "File %s not found or cannot be opened.\n", filename);
         exit(1);
@@ -24,10 +33,11 @@ check_laads_order::check_laads_order(QWidget *parent)
     qDebug() << get_file_cksum(fp);
     fclose(fp);
 }
+
 uint32_t check_laads_order::get_file_cksum(FILE *fp)
 {
 #ifdef Q_OS_WINDOWS
-    #define posix_memalign(p, a, s)  (((*(p)) = _aligned_malloc((s), (a))), *(p) ?0 :errno)
+#define posix_memalign(p, a, s)  (((*(p)) = _aligned_malloc((s), (a))), *(p) ?0 :errno)
 #endif
     const uint64_t BUFSIZE = 64 << 10;
     void *buffer = NULL;
