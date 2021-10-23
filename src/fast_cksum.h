659f58eefc2a7cbdd950d4fd43816b26bb547f1c
Zheng Xueke <lengfeng1453@163.com>
2021-10-23 21:14:15 +0800

    统一代码风格

--- src/fast_cksum.h
+++ src/fast_cksum.h
@@ -1,5 +1,5 @@
 #ifndef PREFETCH
-#define PREFETCH(mem)  __builtin_prefetch(mem)
+#define PREFETCH(mem) __builtin_prefetch(mem)
 #endif
 
 // uint8_t, uint32_t, int32_t
@@ -11,22 +11,27 @@
 extern "C" {
 #endif
 
-// When starting a series of partial CRC accumulations, use this starting CRC value
+// When starting a series of partial CRC accumulations, use this starting CRC
+// value
 #define CRC32_FAST_SEED 0xFFFFFFFF
 
-// TODO: this is the only place we use the crc32_fast nomenclature instead of fast_cksum
+// TODO: this is the only place we use the crc32_fast nomenclature instead of
+// fast_cksum
 
 /// compute CRC32
-uint32_t crc32_fast(const void* data, size_t length);
+uint32_t crc32_fast(const void *data, size_t length);
 
-/// Compute a CRC32 on a chunk(s) of data and then generate the final CRC32 by combining with the total length
-uint32_t crc32_fast_partial(const void* data, size_t length, uint32_t previousCrc32 = CRC32_FAST_SEED);
+/// Compute a CRC32 on a chunk(s) of data and then generate the final CRC32
+/// by combining with the total length
+uint32_t crc32_fast_partial(const void *data, size_t length,
+                            uint32_t previousCrc32 = CRC32_FAST_SEED);
 uint32_t crc32_fast_finalize(size_t total_length, uint32_t previousCrc32);
 
 // Use a specific CRC32 chunking algorithm
-uint32_t crc32_fast_16bytes(const void* data, size_t length);
+uint32_t crc32_fast_16bytes(const void *data, size_t length);
 // Use a specific partial algorithm
-uint32_t crc32_fast_16bytes_partial(const void* data, size_t length, uint32_t previousCrc32 = CRC32_FAST_SEED);
+uint32_t crc32_fast_16bytes_partial(const void *data, size_t length,
+                                    uint32_t previousCrc32 = CRC32_FAST_SEED);
 
 #ifdef __cplusplus
 }  // extern "C"
