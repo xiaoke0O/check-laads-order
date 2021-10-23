2faa706ebd8fa1bc8f366c19b578e1ebb07b522a
Zheng Xueke <lengfeng1453@163.com>
2021-10-23 21:31:48 +0800

    统一代码风格为CLion

--- src/check_laads_order.cpp
+++ src/check_laads_order.cpp
@@ -7,48 +7,51 @@
 
 #include "./ui_check_laads_order.h"
 #include "fast_cksum.h"
+
 check_laads_order::check_laads_order(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::check_laads_order) {
-  ui->setupUi(this);
-  connect(ui->actionAbout_Qt, &QAction::triggered, qApp,
-          &QApplication::aboutQt);
-  connect(ui->actionCkeck_Order, &QAction::triggered, this,
-          &check_laads_order::search_orders);
+        : QMainWindow(parent), ui(new Ui::check_laads_order) {
+    ui->setupUi(this);
+    connect(ui->actionAbout_Qt, &QAction::triggered, qApp,
+            &QApplication::aboutQt);
+    connect(ui->actionCkeck_Order, &QAction::triggered, this,
+            &check_laads_order::search_orders);
 }
+
 uint32_t check_laads_order::get_file_cksum(FILE *fp) {
 #ifdef Q_OS_WINDOWS
 #define posix_memalign(p, a, s) \
   (((*(p)) = _aligned_malloc((s), (a))), *(p) ? 0 : errno)
 #endif
-  const uint64_t BUFSIZE = 64 << 10;
-  void *buffer = NULL;
-  int ret = posix_memalign(&buffer, 65536, BUFSIZE);
-  if (ret != 0 || buffer == NULL) {
-    fprintf(stderr, "Failed to allocate %" PRIu64 " bytes\n", BUFSIZE);
-    exit(1);
-  }
+    const uint64_t BUFSIZE = 64 << 10;
+    void *buffer = NULL;
+    int ret = posix_memalign(&buffer, 65536, BUFSIZE);
+    if (ret != 0 || buffer == NULL) {
+        fprintf(stderr, "Failed to allocate %" PRIu64 " bytes\n", BUFSIZE);
+        exit(1);
+    }
 
-  uint32_t partial_crc = CRC32_FAST_SEED;
-  size_t totalsize = 0;
-  size_t count;
-  while ((count = fread(buffer, 1, BUFSIZE, fp))) {
-    partial_crc = crc32_fast_partial(buffer, count, partial_crc);
-    totalsize += count;
-  }
+    uint32_t partial_crc = CRC32_FAST_SEED;
+    size_t totalsize = 0;
+    size_t count;
+    while ((count = fread(buffer, 1, BUFSIZE, fp))) {
+        partial_crc = crc32_fast_partial(buffer, count, partial_crc);
+        totalsize += count;
+    }
 
-  uint32_t crc = crc32_fast_finalize(totalsize, partial_crc);
+    uint32_t crc = crc32_fast_finalize(totalsize, partial_crc);
 #ifdef Q_OS_WINDOWS
-  _aligned_free(buffer);
+    _aligned_free(buffer);
 #else
-  free(buffer);
+    free(buffer);
 #endif  // Q_OS_WINDOWS
-  return crc;
+    return crc;
 }
+
 QString check_laads_order::get_orders_directory() {
-  QString dialog_title = tr("Open Orders Directory");
-  QString open_dir =
-      QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
-  return QFileDialog::getExistingDirectory(this, dialog_title, open_dir);
+    QString dialog_title = tr("Open Orders Directory");
+    QString open_dir =
+            QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
+    return QFileDialog::getExistingDirectory(this, dialog_title, open_dir);
 }
 
 void check_laads_order::search_orders() {}
--- src/check_laads_order.h
+++ src/check_laads_order.h
@@ -5,23 +5,27 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class check_laads_order;
+    class check_laads_order;
 }
 QT_END_NAMESPACE
 
 class check_laads_order : public QMainWindow {
-  Q_OBJECT
+Q_OBJECT
 
- public:
-  check_laads_order(QWidget *parent = nullptr);
-  ~check_laads_order();
+public:
+    check_laads_order(QWidget *parent = nullptr);
 
- private:
-  uint32_t get_file_cksum(FILE *fp);
-  QString get_orders_directory();
-  void search_orders();
+    ~check_laads_order();
 
- private:
-  Ui::check_laads_order *ui;
+private:
+    uint32_t get_file_cksum(FILE *fp);
+
+    QString get_orders_directory();
+
+    void search_orders();
+
+private:
+    Ui::check_laads_order *ui;
 };
+
 #endif  // CHECK_LAADS_ORDER_H
