06538cdd44a5361b8635c9a0ccdbb45233075e2f
Zheng Xueke <lengfeng1453@163.com>
2021-11-01 21:55:10 +0800

    add: 创建缺失和错误文件的下载链接并保存为文件

--- src/report.cpp
+++ src/report.cpp
@@ -6,18 +6,28 @@
 
 #include <utility>
 #include "ui_report.h"
+#include <QFileDialog>
+#include <QDebug>
+#include <QMessageBox>
 
-report::report(QString _order_sn,
+report::report(QString _order_dir,
+			   QString _order_sn,
 			   QStringList _error_files,
 			   QStringList _missing_files,
-			   QStringList _extra_files) : order_sn(std::move(_order_sn)),
+			   QStringList _extra_files) : order_dir(std::move(_order_dir)),
+										   order_sn(std::move(_order_sn)),
 										   error_files(std::move(_error_files)),
 										   missing_files(std::move(_missing_files)),
 										   extra_files(std::move(_extra_files)),
 										   ui(new Ui::report) {
   ui->setupUi(this);
   setWindowTitle(tr("Order %1 Check Report").arg(order_sn));
+  ui->pushButton_creat->setDisabled(true);
+  ui->pushButton_delete->setDisabled(true);
   set_content();
+
+  connect(ui->pushButton_creat, &QPushButton::clicked,
+		  this, &report::create_downloadable_files_link);
 }
 
 report::~report() {
@@ -37,13 +47,46 @@ void report::set_content() {
 		  QString("(%1)").arg(QString::number(extra_files.size())));
 
   // 文件内容
-	  foreach(QString text, error_files) ui->textBrowser_error->append(
-		  text);
-	  foreach(QString text, missing_files) ui->textBrowser_missing->append(
-		  text);
-	  foreach(QString text, extra_files) ui->textBrowser_unwanted->append(
-		  text);
+  if (!error_files.isEmpty() || !missing_files.isEmpty()) {
+	for (auto &text: error_files)
+	  ui->textBrowser_error->append(text);
+	for (auto &text: missing_files)
+	  ui->textBrowser_missing->append(text);
+	ui->pushButton_creat->setEnabled(true);
+  }
+  if (!extra_files.isEmpty()) {
+	for (auto &text: extra_files)
+	  ui->textBrowser_unwanted->append(text);
+	ui->pushButton_delete->setEnabled(true);
+  }
+}
+void report::create_downloadable_files_link() {
+  QStringList links;
+  QString website = "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders";
+  for (auto &file_name: error_files)
+	links << QString("%1/%2/%3").arg(website, order_sn, file_name);
+  for (auto &file_name: missing_files)
+	links << QString("%1/%2/%3").arg(website, order_sn, file_name);
 
+  // 文件保存框
+  QString save_file_file = QString("%1/redownload_file_links.txt").arg(order_dir);
+  QString file_name = QFileDialog::getSaveFileName(this,
+												   tr("Save Download Links"),
+												   save_file_file,
+												   tr("Text (*.txt)"));
+  QFile write_out(file_name);
+  if (write_out.open(QFile::WriteOnly | QFile::Text)) {
+	QTextStream s(&write_out);
+	s << links.join("\n");
+	QMessageBox::information(this, tr("Successfully Save!"),
+							 tr("The download links for the error files and "
+								"missing files have been saved, please download them "
+								"as soon as possible <b>within the validity period of "
+								"the order</b>."));
+  } else {
+	QMessageBox::critical(this, tr("Save Error"), tr("Error writing this file"));
+  }
+  write_out.close();
 }
 
 
--- src/report.h
+++ src/report.h
@@ -16,21 +16,27 @@ QT_END_NAMESPACE
 class report : public QWidget {
  Q_OBJECT
  public:
-  report(QString _order_sn,
-		 QStringList _error_files,
-		 QStringList _missing_files,
-		 QStringList _extra_files);
+  report(QString _order_dir,
+	  QString  _order_sn,
+		 QStringList  _error_files,
+		 QStringList  _missing_files,
+		 QStringList  _extra_files);
 
   ~report() override;
 
  private:
+  QString order_dir;
   QString order_sn;
   QStringList error_files;
   QStringList missing_files;
   QStringList extra_files;
+  Ui::report *ui;
  private:
   void set_content();
-  Ui::report *ui;
+ private slots:
+  void create_downloadable_files_link();
+
+
 };
 
 #endif //CHECK_LAADS_ORDER_REPORT_H
