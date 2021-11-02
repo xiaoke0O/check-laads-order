cc41fcefee7913ffe32ec737ce614305a19c78ff
Zheng Xueke <lengfeng1453@163.com>
2021-11-02 08:54:20 +0800

    fix: 中文逗号

--- src/check_laads_order.cpp
+++ src/check_laads_order.cpp
@@ -91,7 +91,7 @@ check_laads_order::fill_result_cells(bool calculate_status, Order *this_order) {
 	result_cell_text = tr("PASS!");
 	// 如果有错误文件，就变成红色
 	if (this_order->get_check_result()) {
-	  result_cell_color.setRgb(0xff，0x51,0x51);
+	  result_cell_color.setRgb(0xff,0x51,0x51);
 	  result_cell_text = tr("FAIL!");
 	}
   } else {
